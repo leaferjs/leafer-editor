39e01bd043b4e5b6f847ef17351c5f562758e4ec
leaferjs <leaferjs@163.com>
2024-07-20 00:29:13 +0800

    v1.0.1

--- packages/platform/web/src/index.ts
+++ packages/platform/web/src/index.ts
@@ -1,5 +1,6 @@
 export * from 'leafer-ui'
 
 export * from '@leafer-editor/partner'
+export * from '@leafer-in/text-editor'
 export * from '@leafer-in/html'
 
--- packages/platform/worker/src/index.ts
+++ packages/platform/worker/src/index.ts
@@ -1,4 +1,5 @@
 export * from '@leafer-ui/worker'
 
 export * from '@leafer-editor/partner'
+export * from '@leafer-in/text-editor'
 export * from '@leafer-in/html'
