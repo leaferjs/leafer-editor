867df3d42c42fbba4038f8171da7b9c1d2a8503c
leaferjs <leaferjs@163.com>
2024-12-26 09:49:57 +0800

    v1.2.0

--- packages/partner/src/index.ts
+++ packages/partner/src/index.ts
@@ -1,6 +1,7 @@
 export * from '@leafer-in/interface'
 
 export * from '@leafer-in/editor'
+export * from '@leafer-in/viewport'
 export * from '@leafer-in/view'
 export * from '@leafer-in/scroll'
 export * from '@leafer-in/arrow'
